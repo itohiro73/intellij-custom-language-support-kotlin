490a948711ee705a28b2086d3b9b4138114dd3d0
Hiroshi Ito <itohiro73@gmail.com>
2015-09-30 23:59:20 +0900

    Fix icon path and java compilation.

---
+++ src/main/kotlin/jp/itohiro/intellij/simpleplugin/SimpleSyntaxHighlighter.kt
@@ -0,0 +1,54 @@
+package jp.itohiro.intellij.simpleplugin
+
+import com.intellij.lexer.Lexer
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
+import com.intellij.openapi.editor.HighlighterColors
+import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.openapi.fileTypes.SyntaxHighlighterBase
+import com.intellij.psi.TokenType
+import com.intellij.psi.tree.IElementType
+import jp.itohiro.intellij.simpleplugin.psi.SimpleTypes
+
+public class SimpleSyntaxHighlighter: SyntaxHighlighterBase() {
+    override fun getTokenHighlights(tokenType: IElementType?): Array<out TextAttributesKey> {
+        throw UnsupportedOperationException()
+    }
+
+    override fun getHighlightingLexer(): Lexer {
+        throw UnsupportedOperationException()
+    }
+    //    companion object {
+//        public val SEPARATOR = createTextAttributesKey("SIMPLE_SEPARATOR", DefaultLanguageHighlighterColors.OPERATION_SIGN);
+//        public val KEY = createTextAttributesKey("SIMPLE_KEY", DefaultLanguageHighlighterColors.KEYWORD);
+//        public val VALUE = createTextAttributesKey("SIMPLE_VALUE", DefaultLanguageHighlighterColors.STRING);
+//        public val COMMENT = createTextAttributesKey("SIMPLE_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
+//        public val BAD_CHARACTER = createTextAttributesKey("SIMPLE_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER);
+//
+//        private val BAD_CHAR_KEYS = arrayOf(BAD_CHARACTER);
+//        private val SEPARATOR_KEYS = arrayOf(SEPARATOR);
+//        private val KEY_KEYS = arrayOf(KEY);
+//        private val VALUE_KEYS = arrayOf(VALUE);
+//        private val COMMENT_KEYS = arrayOf(COMMENT);
+//        private val EMPTY_KEYS = Array<TextAttributesKey>()
+//    }
+//
+//    override fun getTokenHighlights(tokenType: IElementType?): Array<out TextAttributesKey> {
+//        if (tokenType == SimpleTypes.SEPARATOR) {
+//            return SEPARATOR_KEYS;
+//        } else if (tokenType == SimpleTypes.KEY) {
+//            return KEY_KEYS;
+//        } else if (tokenType == SimpleTypes.VALUE) {
+//            return VALUE_KEYS;
+//        } else if (tokenType == SimpleTypes.COMMENT) {
+//            return COMMENT_KEYS;
+//        } else if (tokenType == TokenType.BAD_CHARACTER) {
+//            return BAD_CHAR_KEYS;
+//        } else {
+//            return EMPTY_KEYS;
+//        }
+//    }
+//
+//    override fun getHighlightingLexer(): Lexer {
+//        return SimpleLexerAdapter()
+//    }
+}

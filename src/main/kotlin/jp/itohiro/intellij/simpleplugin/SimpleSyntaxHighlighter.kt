70bb28ea04f5a2a1d9f2512f995558a7006595da
Hiroshi Ito <itohiro73@gmail.com>
2015-10-01 00:27:43 +0900

    Add syntax highlighter.

--- src/main/kotlin/jp/itohiro/intellij/simpleplugin/SimpleSyntaxHighlighter.kt
+++ src/main/kotlin/jp/itohiro/intellij/simpleplugin/SimpleSyntaxHighlighter.kt
@@ -9,46 +9,44 @@ import com.intellij.psi.TokenType
 import com.intellij.psi.tree.IElementType
 import jp.itohiro.intellij.simpleplugin.psi.SimpleTypes
 
+import com.intellij.openapi.editor.colors.TextAttributesKey.createTextAttributesKey;
+import org.jetbrains.annotations.NotNull
+
 public class SimpleSyntaxHighlighter: SyntaxHighlighterBase() {
-    override fun getTokenHighlights(tokenType: IElementType?): Array<out TextAttributesKey> {
-        throw UnsupportedOperationException()
+    companion object {
+        public val SEPARATOR = createTextAttributesKey("SIMPLE_SEPARATOR", DefaultLanguageHighlighterColors.OPERATION_SIGN)
+        public val KEY = createTextAttributesKey("SIMPLE_KEY", DefaultLanguageHighlighterColors.KEYWORD)
+        public val VALUE = createTextAttributesKey("SIMPLE_VALUE", DefaultLanguageHighlighterColors.STRING)
+        public val COMMENT = createTextAttributesKey("SIMPLE_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT)
+        public val BAD_CHARACTER = createTextAttributesKey("SIMPLE_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER)
+
+        private val BAD_CHAR_KEYS = arrayOf(BAD_CHARACTER)
+        private val SEPARATOR_KEYS = arrayOf(SEPARATOR)
+        private val KEY_KEYS = arrayOf(KEY)
+        private val VALUE_KEYS = arrayOf(VALUE)
+        private val COMMENT_KEYS = arrayOf(COMMENT)
+        private val EMPTY_KEYS = arrayOf<TextAttributesKey>()
     }
 
-    override fun getHighlightingLexer(): Lexer {
-        throw UnsupportedOperationException()
+    @NotNull
+    override fun getTokenHighlights(tokenType: IElementType?): Array<out TextAttributesKey> {
+        if (tokenType == SimpleTypes.SEPARATOR) {
+            return SEPARATOR_KEYS;
+        } else if (tokenType == SimpleTypes.KEY) {
+            return KEY_KEYS;
+        } else if (tokenType == SimpleTypes.VALUE) {
+            return VALUE_KEYS;
+        } else if (tokenType == SimpleTypes.COMMENT) {
+            return COMMENT_KEYS;
+        } else if (tokenType == TokenType.BAD_CHARACTER) {
+            return BAD_CHAR_KEYS;
+        } else {
+            return EMPTY_KEYS;
+        }
     }
-    //    companion object {
-//        public val SEPARATOR = createTextAttributesKey("SIMPLE_SEPARATOR", DefaultLanguageHighlighterColors.OPERATION_SIGN);
-//        public val KEY = createTextAttributesKey("SIMPLE_KEY", DefaultLanguageHighlighterColors.KEYWORD);
-//        public val VALUE = createTextAttributesKey("SIMPLE_VALUE", DefaultLanguageHighlighterColors.STRING);
-//        public val COMMENT = createTextAttributesKey("SIMPLE_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
-//        public val BAD_CHARACTER = createTextAttributesKey("SIMPLE_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER);
-//
-//        private val BAD_CHAR_KEYS = arrayOf(BAD_CHARACTER);
-//        private val SEPARATOR_KEYS = arrayOf(SEPARATOR);
-//        private val KEY_KEYS = arrayOf(KEY);
-//        private val VALUE_KEYS = arrayOf(VALUE);
-//        private val COMMENT_KEYS = arrayOf(COMMENT);
-//        private val EMPTY_KEYS = Array<TextAttributesKey>()
-//    }
-//
-//    override fun getTokenHighlights(tokenType: IElementType?): Array<out TextAttributesKey> {
-//        if (tokenType == SimpleTypes.SEPARATOR) {
-//            return SEPARATOR_KEYS;
-//        } else if (tokenType == SimpleTypes.KEY) {
-//            return KEY_KEYS;
-//        } else if (tokenType == SimpleTypes.VALUE) {
-//            return VALUE_KEYS;
-//        } else if (tokenType == SimpleTypes.COMMENT) {
-//            return COMMENT_KEYS;
-//        } else if (tokenType == TokenType.BAD_CHARACTER) {
-//            return BAD_CHAR_KEYS;
-//        } else {
-//            return EMPTY_KEYS;
-//        }
-//    }
-//
-//    override fun getHighlightingLexer(): Lexer {
-//        return SimpleLexerAdapter()
-//    }
+
+    @NotNull
+    override fun getHighlightingLexer(): Lexer {
+        return SimpleLexerAdapter()
+    }
 }
---
+++ src/main/kotlin/jp/itohiro/intellij/simpleplugin/SimpleSyntaxHighlighterFactory.kt
@@ -0,0 +1,14 @@
+package jp.itohiro.intellij.simpleplugin
+
+import com.intellij.openapi.fileTypes.SyntaxHighlighter
+import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import org.jetbrains.annotations.NotNull
+
+public class SimpleSyntaxHighlighterFactory: SyntaxHighlighterFactory() {
+    @NotNull
+    override fun getSyntaxHighlighter(project: Project?, virtualFile: VirtualFile?): SyntaxHighlighter {
+        return SimpleSyntaxHighlighter()
+    }
+}

81e5ecf17f707a9b54ae49890cd7adce83d7f598
Hiroshi Ito <itohiro73@gmail.com>
2015-10-09 22:51:32 +0900

    Add Annotator (WIP).

---
+++ src/main/kotlin/jp/itohiro/intellij/simpleplugin/SimpleAnnotator.kt
@@ -0,0 +1,31 @@
+package jp.itohiro.intellij.simpleplugin
+
+import com.intellij.lang.annotation.AnnotationHolder
+import com.intellij.lang.annotation.Annotator
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiLiteralExpression
+
+public class SimpleAnnotator: Annotator {
+    override fun annotate(element: PsiElement, holder: AnnotationHolder) {
+        if(element is PsiLiteralExpression) {
+            val literalExpression: PsiLiteralExpression = element
+            val value = literalExpression.value as String
+
+            if (value.startsWith("simple:")) {
+                val project = element.project
+                val key = value.substring(7)
+                val properties = SimpleUtil.findProperties(project, key)
+                if (properties?.size() == 1) {
+                    val range = TextRange(element.textRange.startOffset + 7, element.textRange.startOffset + 7)
+                    val annotation = holder.createInfoAnnotation(range, null)
+                    annotation.textAttributes = DefaultLanguageHighlighterColors.LINE_COMMENT
+                } else if (properties?.size() == 0) {
+                    val range = TextRange(element.textRange.startOffset, element.textRange.startOffset + 8)
+                    holder.createErrorAnnotation(range, "Unresolved property")
+                }
+            }
+        }
+    }
+}
